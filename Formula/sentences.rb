5f3fd8d4fae040ecd1cfbb63730f75a14f4382e9
Eric Bower <me@erock.io>
2021-06-18 11:49:39 -0400

    v1.0.9

--- Formula/sentences.rb
+++ Formula/sentences.rb
@@ -4,6 +4,7 @@ class Sentences < Formula
   url "https://github.com/neurosnap/sentences/releases/download/v1.0.9/sentences_darwin-amd64.tar.gz"
   sha256 "74541e12c0c30ff3ea499419d1bf981462325eaf56fe0c0380e4778b9ab2fc94"
   license "MIT"
+  version "1.0.9"
 
   def install
       libexec.install Dir["*"]
