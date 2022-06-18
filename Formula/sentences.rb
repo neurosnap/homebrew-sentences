e39e57f1032e5e8dfba77a18623082b1920f05c3
Eric Bower <me@erock.io>
2022-06-18 13:07:45 -0400

    update version

--- Formula/sentences.rb
+++ Formula/sentences.rb
@@ -1,10 +1,10 @@
 class Sentences < Formula
   desc "A multilingual command line sentence tokenizer"
   homepage "https://sentences-231000.appspot.com/"
-  url "https://github.com/neurosnap/sentences/releases/download/v1.0.9/sentences_darwin-amd64.tar.gz"
-  sha256 "74541e12c0c30ff3ea499419d1bf981462325eaf56fe0c0380e4778b9ab2fc94"
+  url "https://github.com/neurosnap/sentences/releases/download/v1.0.10/sentences_darwin-amd64.tar.gz"
+  sha256 "12e561c6882d9122b483a3edd415e120a8bf4bcff8384648672ca43a1417545d"
   license "MIT"
-  version "1.0.9"
+  version "1.0.10"
 
   def install
       libexec.install Dir["*"]
