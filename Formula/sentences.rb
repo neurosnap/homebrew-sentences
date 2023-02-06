6e63445173b73f6d3c636126745914488d869d89
Eric Bower <me@erock.io>
2023-02-06 09:44:15 -0500

    chore: bump to 1.1.2

--- Formula/sentences.rb
+++ Formula/sentences.rb
@@ -2,16 +2,16 @@ class Sentences < Formula
   desc "A command line sentence tokenizer"
   homepage "https://sentences-231000.appspot.com"
   license "MIT"
-  version "1.1.1"
+  version "1.1.2"
 
   on_arm do
-    url "https://github.com/neurosnap/sentences/releases/download/v1.1.1/sentences_darwin-arm64.tar.gz"
-    sha256 "078762aa6a92e4fca013c8163e65ebd7f6cde6425a617df5accc57c777abe76b"
+    url "https://github.com/neurosnap/sentences/releases/download/v1.1.2/sentences_darwin-arm64.tar.gz"
+    sha256 "a2d51bd9cc8aacb2fb70c62a4df76a3041cba1d8131d0a536725227b0231e8b2"
   end
 
   on_intel do
-    url "https://github.com/neurosnap/sentences/releases/download/v1.1.1/sentences_darwin-amd64.tar.gz"
-    sha256 "5ffa6695e70510b97d7c6f84672039b7d797957ab0e663ce68605fc1a2c94531"
+    url "https://github.com/neurosnap/sentences/releases/download/v1.1.2/sentences_darwin-amd64.tar.gz"
+    sha256 "b7261dd1b9c4d0a9a747c4eaa5433b9d5cdd0b5e71955c5eb2be48ca6fd2cfc8"
   end
 
   def install
